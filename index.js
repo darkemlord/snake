92fa448eaca76989e7380bd04e80575e5ce35d87
emanuel caro <darkemlord@gmail.com>
2023-03-16 19:51:58 +0900

    refs 001 drawing board

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const hello = () => {
-  return "hello";
+window.onload = () => {
+    const canvas = document.getElementById("game-container");
+    if (canvas) {
+        const ctx = canvas.getContext("2d");
+        if (ctx) {
+            canvas.width = 500;
+            canvas.height = 500;
+            canvas.style.backgroundColor = "white";
+            const divSize = 50;
+            const numCols = canvas.width / divSize;
+            const numRows = canvas.height / divSize;
+            const divs = [];
+            for (let i = 0; i < numCols; i++) {
+                divs[i] = [];
+                for (let j = 0; j < numRows; j++) {
+                    const div = document.createElement("div");
+                    div.style.width = `${divSize}px`;
+                    div.style.height = `${divSize}px`;
+                    div.style.backgroundColor = "black";
+                    divs[i][j] = div;
+                }
+            }
+            for (let i = 0; i < numCols; i++) {
+                for (let j = 0; j < numRows; j++) {
+                    const img = new Image();
+                    img.src = `data:image/svg+xml,${encodeURIComponent(divs[i][j].outerHTML)}`;
+                    img.onload = () => {
+                        ctx.drawImage(img, i * divSize, j * divSize, divSize, divSize);
+                    };
+                }
+            }
+        }
+    }
 };
-console.log(hello());
--- index.ts
+++ index.ts
@@ -1,5 +1,44 @@
-const hello = (): string => {
-  return "hello";
-};
+window.onload = () => {
+  const canvas = document.getElementById("game-container") as HTMLCanvasElement;
 
-console.log(hello());
+  if (canvas) {
+    const ctx = canvas.getContext("2d");
+
+    if (ctx) {
+      canvas.width = 500;
+      canvas.height = 500;
+      canvas.style.backgroundColor = "white";
+
+      const divSize = 50;
+      const numCols = canvas.width / divSize;
+      const numRows = canvas.height / divSize;
+
+      const divs: HTMLDivElement[][] = [];
+
+      for (let i = 0; i < numCols; i++) {
+        divs[i] = [];
+
+        for (let j = 0; j < numRows; j++) {
+          const div = document.createElement("div");
+          div.style.width = `${divSize}px`;
+          div.style.height = `${divSize}px`;
+          div.style.backgroundColor = "black";
+          divs[i][j] = div;
+        }
+      }
+
+      for (let i = 0; i < numCols; i++) {
+        for (let j = 0; j < numRows; j++) {
+          const img = new Image();
+          img.src = `data:image/svg+xml,${encodeURIComponent(
+            divs[i][j].outerHTML
+          )}`;
+
+          img.onload = () => {
+            ctx.drawImage(img, i * divSize, j * divSize, divSize, divSize);
+          };
+        }
+      }
+    }
+  }
+};
